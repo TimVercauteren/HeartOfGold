065d6f0ece85ec68b22c3fed4225d72ea3d22f73
Tim Vercauteren <tim.vercauteren@gmail.com>
2017-09-11 21:40:33 +0200

    vm

--- BudgetMiner/Business/Models/EmployeeCost/EmployeeCostModel.cs
+++ BudgetMiner/Business/Models/EmployeeCost/EmployeeCostModel.cs
@@ -9,14 +9,25 @@ namespace BudgetMiner.Business.Models.EmployeeCost
 {
     public class EmployeeCostModel
     {
+        [DataType(DataType.Text)]
+        [MaxLength(15)]
         [Required]
         public string FunctionTitle { get; set; }
         [Required]
         public string FunctionDescription { get; set; }
+        [DataType(DataType.Currency)]
+        [Required]
         public decimal BaseJunior { get; set; }
+        [DataType(DataType.Currency)]
+        [Required]
         public decimal BaseMedior { get; set; }
+        [DataType(DataType.Currency)]
+        [Required]
         public decimal BaseSenior { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
         public decimal AnnualIncrease { get; set; }
+        [Required]
         public string InstructionsHTML { get; set; }
     }
 }
