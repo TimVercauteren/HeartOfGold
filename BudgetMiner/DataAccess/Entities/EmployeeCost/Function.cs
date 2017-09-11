1c3f8f88077238a429569ea23ae79f532718ba75
Tim Vercauteren <tim.vercauteren@gmail.com>
2017-09-11 21:30:19 +0200

    employee class

---
+++ BudgetMiner/DataAccess/Entities/EmployeeCost/Function.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BudgetMiner.DataAccess.Entities.EmployeeCost
+{
+    public class Function
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal BaseJunior { get; set; }
+        public decimal BaseMedior { get; set; }
+        public decimal BaseSenior { get; set; }
+        public decimal AnnualIncrease { get; set; }
+    }
+}
