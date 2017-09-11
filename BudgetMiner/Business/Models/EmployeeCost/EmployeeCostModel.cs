e1df6efc58c731a67256f88ba97e891324d10640
Tim Vercauteren <tim.vercauteren@gmail.com>
2017-09-11 21:33:19 +0200

    add VM

---
+++ BudgetMiner/Business/Models/EmployeeCost/EmployeeCostModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetMiner.Business.Models.EmployeeCost
+{
+    public class EmployeeCostModel
+    {
+        [Required]
+        public string FunctionTitle { get; set; }
+        [Required]
+        public string FunctionDescription { get; set; }
+        public decimal BaseJunior { get; set; }
+        public decimal BaseMedior { get; set; }
+        public decimal BaseSenior { get; set; }
+        public decimal AnnualIncrease { get; set; }
+        public string InstructionsHTML { get; set; }
+    }
+}
