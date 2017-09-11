e600167d2f3d0b9aaeab3cb975500075c6af1309
Tim Vercauteren <tim.vercauteren@gmail.com>
2017-09-11 20:49:44 +0200

    add basic models

---
+++ BudgetMiner/DataAccess/Entities/_base/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetMiner.DataAccess.Entities
+{
+    public class EntityBase
+    {
+        [Required]
+        int Id { get; set; }
+    }
+}
